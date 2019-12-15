0ff507362f9dc30bfcd8441e29baa7ef4edab56a
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2019-12-15 13:37:11 +0100

    Added new win condition

--- src/index.js
+++ src/index.js
@@ -154,6 +154,7 @@ function Square(props) {
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6],
+      [0, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
